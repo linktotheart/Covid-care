f654b9890b4c7afdd03012c3b23b07f45dcfd1b2
Pulkit Singh <44489787+pulkitsingh69@users.noreply.github.com>
2020-11-13 13:09:27 +0530

    All pages

--- js/precult.js
+++ js/precult.js
@@ -31,6 +31,15 @@ $(document).ready(function(){
 		$(".search-form-container").toggleClass("active");
 		$(this).children().children(".fa").toggleClass("fa-search fa-remove ");
 	})
+
+
+	///////////////// filter toggle
+	$('#filter-button, .f-close.btn').click(function(){
+		$('#filter-wrap').toggleClass("active");
+		$("body").toggleClass('scroll-lock');
+	})
+
+	
 })
 
 /* menu sliding */
@@ -64,7 +73,7 @@ $(document).on('click', '.dropdown-menu', function (e) {
 });
 
 ///// featured slider ///
-$('#featured-product-slider').owlCarousel({
+$('#featured-product-slider, #related-products').owlCarousel({
     loop:false,
 	margin:0,
 	dots: false,
@@ -90,7 +99,7 @@ $('#featured-product-slider').owlCarousel({
 $('#hero-slider').owlCarousel({
 	loop: true,
 	dots: false,
-	nav: true,
+	nav: false,
 	items: 1,
 	margin: 5,
 	animateOut: 'fadeOut',
@@ -99,27 +108,139 @@ $('#hero-slider').owlCarousel({
 	autoplayTimeout:5000,
 	autoplayHoverPause:false,
 	navText:["<i class='fa fa-angle-left '></i>","<i class='fa fa-angle-right '></i>"],
-})
+	responsive:{
+        576:{
+			nav:false,
+			dots: true
+		},
+		768:{
+			nav: true,
+			dots: false
+		},
+		1140:{
+			dots: false,
+			nav: true
+        }
+	}
+});
 
 ////// product img slider /////////
 $('#product-img-slider').owlCarousel({
 	loop: true,
-	margin: 0,
-	dots: false,
+	margin: 10,
+	dots: true,
 	nav: false,
 	autoHeight: true,
 	items: 1,
+	center: true,
     thumbs: true,
-    thumbsPrerendered: true})
+	thumbsPrerendered: true,
+	responsive:{
+        576:{
+			nav:false,
+			dots: true
+		},
+		768:{
+			dots: false
+		},
+		1140:{
+			dots: false,
+        }
+	}});
 
-// Increase/Reduce product amount
-$('.dec-btn').click(function () {
-	var siblings = $(this).siblings('input.quantity-no');
-	if (parseInt(siblings.val(), 10) >= 1) {
-		siblings.val(parseInt(siblings.val(), 10) - 1);
+$(document).ready(function(){
+
+	var current_fs, next_fs, previous_fs; //fieldsets
+	var opacity;
+	
+	$(".next").click(function(){
+	
+	current_fs = $(this).parent();
+	next_fs = $(this).parent().next();
+	
+	//Add Class Active
+	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+	
+	//show the next fieldset
+	next_fs.show();
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+	step: function(now) {
+	// for making fielset appear animation
+	opacity = 1 - now;
+	
+	current_fs.css({
+	'display': 'none',
+	'position': 'relative'
+	});
+	next_fs.css({'opacity': opacity});
+	},
+	duration: 600
+	});
+	});
+	
+	$(".previous").click(function(){
+	
+	current_fs = $(this).parent();
+	previous_fs = $(this).parent().prev();
+	
+	//Remove class active
+	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+	
+	//show the previous fieldset
+	previous_fs.show();
+	
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+	step: function(now) {
+	// for making fielset appear animation
+	opacity = 1 - now;
+	
+	current_fs.css({
+	'display': 'none',
+	'position': 'relative'
+	});
+	previous_fs.css({'opacity': opacity});
+	},
+	duration: 600
+	});
+	});
+	
+	$('.radio-group .radio').click(function(){
+	$(this).parent().find('.radio').removeClass('selected');
+	$(this).addClass('selected');
+	});
+	
+	$(".submit").click(function(){
+	return false;
+	})
+	
+});
+//////////// zoomer ////////// 
+// $('.zoomer').imgZoom({
+// 	boxWidth: 360,
+// 	boxHeight: 360,
+// 	marginLeft: 15,
+// 	marginTop: 25,
+// 	origin: 'src'
+//   });
+
+/////////// login page 
+function toggleResetPswd(e){
+	e.preventDefault();
+	$('#logreg-forms .form-signin').toggle()
+	$('#logreg-forms .form-reset').toggle() 
+}
+	function toggleSignUp(e){
+		e.preventDefault();
+		$('#logreg-forms .form-signin').toggle(); 
+		$('#logreg-forms .form-signup').toggle(); 
 	}
-});
-$('.inc-btn').click(function () {
-	var siblings = $(this).siblings('input.quantity-no');
-	siblings.val(parseInt(siblings.val(), 10) + 1);
-});
+
+	$(()=>{
+	// Login Register Form
+	$('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+	$('#logreg-forms #cancel_reset').click(toggleResetPswd);
+	$('#logreg-forms #btn-signup').click(toggleSignUp);
+	$('#logreg-forms #cancel_signup').click(toggleSignUp);
+})
